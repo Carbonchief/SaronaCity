950435ef68c4115ce748562937ac47a46c2fe3da
Luan van der Walt <luan@bfn.co.za>
2023-08-22 13:09:34 +0200

    Updates

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,100 +1,75 @@
 <?php
-/*
-Name: 			Contact Form
-Written by: 	Okler Themes - (http://www.okler.net)
-Theme Version:	9.3.0
-*/
+$firstname = stripslashes($_POST['firstname']);
+$message = stripslashes($_POST['message']);
+$visitor_email = stripslashes($_POST['email']);
+$response = $_POST["captcha"];
+echo "<p>Please complete the captcha</p>";
+//Recaptcha validation
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$data = array(
+    'secret' => '6LeXATglAAAAAKUCEAxF8rfJn7ZUTx__y0M9MM7O',
+    'response' => $_POST["captcha"]
+);
+$options = array(
+    'http' => array(
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context = stream_context_create($options);
+$verify = file_get_contents($url, false, $context);
+$captcha_success = json_decode($verify);
 
-namespace PortoContactForm;
+if ($captcha_success->success == false) {
+    echo "<p>Please complete the captcha</p>";
+} else if ($captcha_success->success == true) {
+    //Validate first
+    if (empty($firstname) || empty($visitor_email)) {
+        echo "Name and email are mandatory!";
+        exit;
+    }
 
-session_cache_limiter('nocache');
-header('Expires: ' . gmdate('r', 0));
+    if (IsInjected($visitor_email)) {
+        echo "Bad email value!";
+        exit;
+    }
 
-header('Content-type: application/json');
+    $email_from = 'web@saronacity.co.bw'; //<== update the email address
+    $email_subject = "New Sarona City Message";
+    $email_body = "You have received a new message from:\n $firstname \n\n Contact Details:\n $visitor_email \n\n Message:\n $message";
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'php-mailer/src/PHPMailer.php';
-require 'php-mailer/src/SMTP.php';
-require 'php-mailer/src/Exception.php';
-
-// Step 1 - Enter your email address below.
-$email = 'you@domain.com';
-
-// If the e-mail is not working, change the debug option to 2 | $debug = 2;
-$debug = 0;
-
-// If contact form don't has the subject input change the value of subject here
-$subject = ( isset($_POST['subject']) ) ? $_POST['subject'] : 'Define subject in php/contact-form.php line 29';
-
-$message = '';
-
-foreach($_POST as $label => $value) {
-	$label = ucwords($label);
-
-	// Use the commented code below to change label texts. On this example will change "Email" to "Email Address"
-
-	// if( $label == 'Email' ) {               
-	// 	$label = 'Email Address';              
-	// }
-
-	// Checkboxes
-	if( is_array($value) ) {
-		// Store new value
-		$value = implode(', ', $value);
-	}
-
-	$message .= $label.": " . nl2br(htmlspecialchars($value, ENT_QUOTES)) . "<br>";
+    $to = "vanderwaltluan@gmail.com"; //<== update the email address
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    //Send the email!
+    mail($to, $email_subject, $email_body, $headers);
+    //done.
+    echo "Thank you for your enquiry.";
 }
 
-$mail = new PHPMailer(true);
 
-try {
 
-	$mail->SMTPDebug = $debug;                                 // Debug Mode
 
-	// Step 2 (Optional) - If you don't receive the email, try to configure the parameters below:
 
-	//$mail->IsSMTP();                                         // Set mailer to use SMTP
-	//$mail->Host = 'mail.yourserver.com';				       // Specify main and backup server
-	//$mail->SMTPAuth = true;                                  // Enable SMTP authentication
-	//$mail->Username = 'user@example.com';                    // SMTP username
-	//$mail->Password = 'secret';                              // SMTP password
-	//$mail->SMTPSecure = 'tls';                               // Enable encryption, 'ssl' also accepted
-	//$mail->Port = 587;   								       // TCP port to connect to
-
-	$mail->AddAddress($email);	 						       // Add another recipient
-
-	//$mail->AddAddress('person2@domain.com', 'Person 2');     // Add a secondary recipient
-	//$mail->AddCC('person3@domain.com', 'Person 3');          // Add a "Cc" address. 
-	//$mail->AddBCC('person4@domain.com', 'Person 4');         // Add a "Bcc" address. 
-
-	// From - Name
-	$fromName = ( isset($_POST['name']) ) ? $_POST['name'] : 'Website User';
-	$mail->SetFrom($email, $fromName);
-
-	// Repply To
-	if( isset($_POST['email']) && !empty($_POST['email']) ) {
-		$mail->AddReplyTo($_POST['email'], $fromName);
-	}
-
-	$mail->IsHTML(true);                                       // Set email format to HTML
-
-	$mail->CharSet = 'UTF-8';
-
-	$mail->Subject = $subject;
-	$mail->Body    = $message;
-
-	$mail->Send();
-	$arrResult = array ('response'=>'success');
-
-} catch (Exception $e) {
-	$arrResult = array ('response'=>'error','errorMessage'=>$e->errorMessage());
-} catch (\Exception $e) {
-	$arrResult = array ('response'=>'error','errorMessage'=>$e->getMessage());
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+    $injections = array(
+        '(\n+)',
+        '(\r+)',
+        '(\t+)',
+        '(%0A+)',
+        '(%0D+)',
+        '(%08+)',
+        '(%09+)'
+    );
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    if (preg_match($inject, $str)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-if ($debug == 0) {
-	echo json_encode($arrResult);
-}
+?>
